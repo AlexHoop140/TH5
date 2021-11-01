8bec252617a462518d5d093e2c8c0895480164f5
AlexHoop140 <alexhoop.pro@gmail.com>
2021-11-01 23:57:38 +0700

    tagpr, del tree

--- tree_bt8.cpp
+++ tree_bt8.cpp
@@ -62,16 +62,17 @@ void preOrderWithoutRecursion(Node *root){
     }
 }
 
-void tagParent(Node *&root){ 
+void tagParent(Node *root){ 
     if(root != NULL){
-        if(rightChild(root) != NULL || leftChild(root) != NULL){
-            root->left->parent = root->right->parent =root; 
-        
+        if(rightChild(root) != NULL){
+            root->right->parent = root; 
+        };
+        if(leftChild(root) != NULL){
+            root->left->parent = root; 
         };
         tagParent(root->left);
         tagParent(root->right);
-    }   
-
+    }
 }
 
 Node *locateNode(struct Node* node, int key)
@@ -90,20 +91,18 @@ Node *locateNode(struct Node* node, int key)
         return NULL;
     }
 }
-//Cho vao node chu kh phai dataType, sua lai 
-// void checkRelative(Node *root, Node *n, Node *m){
-//     // Node *p = locateNode(root, n);
-//     // Node *q = locateNode(root, m);
-//     if((n != NULL) && (n->parent == m->parent)){
-//         cout << "True";
-//     }else 
-//         cout << "False";
-// }
 
-//Cho vao node chu kh phai dataType, sua lai 
-bool checkAncestor(Node *n, Node *m){
-    Node *p = n;
+bool checkDNA(Node *root, Node *n, Node *m){
+    if((n != NULL && m != NULL) && (n->parent == m->parent)){
+        return true;
+    }else 
+        return false;
+}
+
+bool checkAncestor(Node *p, Node *m){
+    // Node *p = n;
     if(p != NULL){
+        // cout << "p data: " << p->Data << endl;
         if(p->Data == m->Data){
             return true;
         } else{
@@ -135,7 +134,28 @@ int countLeaves(Node *root){
     }
 };
 
+void deleteTree(Node* node)
+{
+    if (node == NULL) return;
+
+    deleteTree(node->left);
+    deleteTree(node->right);
+     
+    delete node;
+}
+
 int main(){
+/*
+
+                     0
+                  /      \
+                1           2
+               /  \          / \
+              3    6        8    12
+            /  \   / \     / \
+           4    5  7  13  9  10
+
+*/
     Node *root = newNode(0);
     root->left = newNode(1);
     root->right = newNode(2);
@@ -150,29 +170,22 @@ int main(){
     root->right->left->left = newNode(9);
     root->right->left->right = newNode(10);
 
-    // Node* root = newNode(10);
-    // root->left = newNode(8);
-    // root->right = newNode(2);
-    // root->left->left = newNode(3);
-    // root->left->right = newNode(5);
 
-    // preOrderWithoutRecursion(root);
     tagParent(root);
-    cout << endl;
-    // checkRelative(root, 8, 12);
-    cout << endl;
-    if (checkAncestor(locateNode(root, 7), locateNode(root, 1)) == true)
-        cout << "Co ancestor";
-    else
-        cout << "Khum coa ";
-    cout << endl;
-    cout << countNodeWithOneLeaf(root);
-    cout << endl;
-    cout << countLeaves(root);
-    // cout << root->right->left->parent->Data << endl;
-    // cout << root->right->right->parent->Data << endl;
-    // cout << getParent(root, 8) << " " << getParent(root, 12);
-    // preOrder(root->right);
-    
 
+    if (checkDNA(root, locateNode(root, 1), locateNode(root, 2)) == true){
+        cout << "Those nodes have the same father node" << endl;
+    } else
+        cout << "Those nodes DO NOT have the same father node" << endl;
+
+    if (checkAncestor(locateNode(root, 9), locateNode(root, 1)) == true)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    cout << "Number of node with only one leaf: " << countNodeWithOneLeaf(root);
+    cout << endl;
+    cout << "Number of leaves: " << countLeaves(root);
+
+    deleteTree(root);
 }
